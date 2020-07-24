edf4e8cf191b204dd86c3d789d47961608951116
ijgnd <fooijgnd@barijgnd>
2020-07-24 16:22:23 +0200

    move licensing info

--- src/addTable.py
+++ src/addTable.py
@@ -1,27 +1,3 @@
-"""
-Addon for Anki 2.1 that inserts tables
-this is a modification and extension of the table function from neftas' Power Format Pack
-
-
-Copyright: 2018- ijgnd
-           2014-2017 Stefan van den Akker <neftas@protonmail.com>
-
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
-
 import json
 import os
 import re
