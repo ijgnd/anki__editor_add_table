5bf4ee041c1f6feab7f4c36831f24c087b84419b
ijgnd <fooijgnd@barijgnd>
2020-07-24 17:44:53 +0200

    prepare new update_config, but disable it

--- src/update_config.py
+++ src/update_config.py
@@ -2,18 +2,12 @@ from aqt import mw
 
 from anki.hooks import addHook
 
+from .config import gc, wcs, wcm
+
 
 # check and maybe transform config.json: old V3 to tableaddon_configlevel_2020-04-27
-def maybe_adjust_config():
-    conf = mw.addonManager.getConfig(__name__)
-    if not conf:
-        return
-    if conf.get("tableaddon_configlevel_2020-04-27", None):
-        return
-    if not "table_style_css_V3" in conf:
-        return
-    styling = conf.get("table_style_css_V3")
-    originalstyling = styling.copy()
+def adjust_to_20200427(conf):
+    styling = gc("table_style_css_V3")
     if not isinstance(styling, dict):
         return
     newnames = {
@@ -56,4 +50,99 @@ def maybe_adjust_config():
 
     conf["tableaddon_configlevel_2020-04-27"] = True
     mw.addonManager.writeConfig(__name__, conf)
-addHook('profileLoaded', maybe_adjust_config)
+    return conf
+
+
+default_v3_from_april_2020 = {
+        "css1": {
+            "BODY_STYLING": "",
+            "HEAD_STYLING": "",
+            "TABLE_STYLING": " class='table_class_one' "
+        },
+        "css2": {
+            "BODY_STYLING": "",
+            "HEAD_STYLING": "",
+            "TABLE_STYLING": " class='table_class_two' "
+        },
+        "basic - full width": {
+            "BODY_STYLING": " style='{0} padding: 2px; border: 1px solid;' ",
+            "HEAD_STYLING": " style='{0} padding: 2px; border: 1px solid;' ",
+            "TABLE_STYLING": " class='table_class_basic_full_width' style='font-size: 85%; width: 100%; border-collapse: collapse; border: 1px solid;' "
+        },
+        "basic - minimal width": {
+            "BODY_STYLING": " style='{0} padding:2px; border: 1px solid;' ",
+            "HEAD_STYLING": " style='{0} padding:2px; border: 1px solid;' ",
+            "TABLE_STYLING": " class='table_class_basic_minimal_width' style='font-size: 85%; border-collapse: collapse; border: 1px solid;' "
+        },
+        "no outside border": {
+            "BODY_STYLING": " style='{0} padding: 2px; border: 1px solid' ",
+            "HEAD_STYLING": " style='{0} padding: 2px; border: 1px solid' ",
+            "TABLE_STYLING": " class='table_class_no_outside_border' style='font-size: 85%; width: 100%; border-style: hidden; border-collapse: collapse;' "
+        },
+        "pfp - style": {
+            "BODY_STYLING": " style='{0} padding: 5px; border-bottom: 1px solid #B0B0B0' ",
+            "HEAD_STYLING": " style='{0} padding: 5px; border-bottom: 2px solid #00B3FF' ",
+            "TABLE_STYLING": " class='table_class_pfp_style' style='font-size: 95%; width: 100%; border-collapse: collapse;' "
+        },
+        "unstyled": {
+            "BODY_STYLING": "",
+            "HEAD_STYLING": "",
+            "TABLE_STYLING": ""
+        }
+    }
+
+
+default_v3_from_july_2020 = {
+        "css1": {
+            "BODY_STYLING": "",
+            "HEAD_STYLING": "",
+            "TABLE_STYLING": " class='table_class_one' "
+        },
+        "css2": {
+            "BODY_STYLING": "",
+            "HEAD_STYLING": "",
+            "TABLE_STYLING": " class='table_class_two' "
+        },
+        "basic - full width": {
+            "BODY_STYLING": " style='{0} padding: 2px; border: 1px solid;' ",
+            "HEAD_STYLING": " style='{0} padding: 2px; border: 1px solid;' ",
+            "TABLE_STYLING": " class='table_class_basic_full_width' style='font-size: 85%; width: 100%; border-collapse: collapse; border: 1px solid;' "
+        },
+        "basic - minimal width": {
+            "BODY_STYLING": " style='{0} padding:2px; border: 1px solid;' ",
+            "HEAD_STYLING": " style='{0} padding:2px; border: 1px solid;' ",
+            "TABLE_STYLING": " class='table_class_basic_minimal_width' style='font-size: 85%; border-collapse: collapse; border: 1px solid;' "
+        },
+        "no outside border": {
+            "BODY_STYLING": " style='{0} padding: 2px; border: 1px solid' ",
+            "HEAD_STYLING": " style='{0} padding: 2px; border: 1px solid' ",
+            "TABLE_STYLING": " class='table_class_no_outside_border' style='font-size: 85%; width: 100%; border-style: hidden; border-collapse: collapse;' "
+        },
+        "pfp - style": {
+            "BODY_STYLING": " style='{0} padding: 5px; border-bottom: 1px solid #B0B0B0' ",
+            "HEAD_STYLING": " style='{0} padding: 5px; border-bottom: 2px solid #00B3FF' ",
+            "TABLE_STYLING": " class='table_class_pfp_style' style='font-size: 95%; width: 100%; border-collapse: collapse;' "
+        },
+        "unstyled": {
+            "BODY_STYLING": "",
+            "HEAD_STYLING": "",
+            "TABLE_STYLING": ""
+        }
+    }
+
+
+def maybe_adjust_config():
+    conf = mw.addonManager.getConfig(__name__)
+    if not conf:
+        return
+    # add classes to add-ons config from after 2019-11-07 (v3). This update code was 
+    # introduced in 2020-04-24
+    if not conf.get("tableaddon_configlevel_2020-04-27"):
+        if "table_style_css_V3" in conf:
+            conf = adjust_to_20200427(conf)
+    # remove black border. This update code was introduced in 2020-07-24.
+    if conf.get("tableaddon_configlevel_2020-04-27"):
+        if conf.get("table_style_css_V3") == default_v3_from_april_2020:
+            wcm([["table_style_css_V3", default_v3_from_july_2020]])
+# DONT AUTOUPDATE ANYMORE. NOT WORTH THE TIME TO MAKE SURE ITS RISKFREE.
+# addHook('profileLoaded', maybe_adjust_config)
