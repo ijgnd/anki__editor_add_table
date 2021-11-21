c51cb30269306e0a78197ca983075caec9295b8f
ijgnd <does_not_exist@example.com>
2021-11-21 16:48:34 +0100

    no more first run message

--- src/__init__.py
+++ src/__init__.py
@@ -22,4 +22,4 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 from . import addTable
 from . import update_config
 from . import install_time_log
-from . import update_message
+# from . import update_message
